1bad7891c4af5ca346a425877e236da77b6d362c
rawand-hawwari <rawandh3@gmail.com>
2023-10-28 21:09:39 +0300

    bte5

---
+++ Server/server.js
@@ -0,0 +1,24 @@
+const hostname = '127.0.0.1';
+const port = 3000;
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+
+
+const blogController = require("./Controllers/blogController");
+const userController = require("./Controllers/userController");
+
+app.get("/", blogController.getBlog);
+app.get("/blog/:id", blogController.getBlogDetails);
+app.post("/BlogPost", blogController.postBlog);
+app.post("/Signup", userController.registerUser);
+app.post("/Login", userController.loginUser);
+app.get("/User", userController.getUserData);
+
+// app.post('/BlogPost', upload.single('image'), blogController.postBlog);
+
+
+app.listen(port, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
